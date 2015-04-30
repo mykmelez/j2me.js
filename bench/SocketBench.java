8b385c2880eb29f9f43c4b80aacec864fa5e97e6
Michael Bebenita <mbebenita@gmail.com>
2015-04-29 21:40:56 -0700

    Revert "Downloads lots of stuff in parallel and try to lock up the UI thread."
    This reverts commit 38c3de2a9c58d2a4cded2404187bab78143d3777.

--- bench/SocketBench.java
+++ bench/SocketBench.java
@@ -9,11 +9,9 @@ import javax.microedition.io.SocketConnection;
 import javax.microedition.midlet.MIDlet;
 import org.mozilla.MemorySampler;
 
-import javax.microedition.midlet.*;
-import javax.microedition.lcdui.*;
+// This needs to be a midlet in order to have access to the J2ME socket API.
 
-class DoStuff implements Runnable {
-
+public class SocketBench extends MIDlet {
   void benchmarkLargeRead() throws IOException {
     SocketConnection client = (SocketConnection)Connector.open("socket://localhost:8000");
 
@@ -36,60 +34,12 @@ class DoStuff implements Runnable {
     client.close();
   }
 
-  public void run() {
-    while (true) {
-      try {
-        System.out.println("Downloading File On Thread: " + Thread.currentThread().getName());
-        Thread.yield();
-        benchmarkLargeRead();
-      } catch (IOException e) {
-        System.out.println("Exception unexpected: " + e);
-        System.out.println("Make sure the test HTTP server is running: python tests/httpServer.py");
-      }
-    }
-  }
-}
-
-class SocketGame extends Canvas {
-  int x = 0;
-  int y = 0;
-  int dx = 3;
-  int dy = 5;
-  public void paint(Graphics g) {
-    int z = 0;
-
-    while (true) {
-      if ((x + dx) < 0 || (x + dx) > 200) {
-        dx *= -1;
-      }
-      if ((y + dy) < 0 || (y + dy) > 200) {
-        dy *= -1;
-      }
-      x += dx;
-      y += dy;
-      g.setColor((z + x) & 0xff, (z + y) & 0xff, (x + y) & 0xff);
-      g.fillRect(x, y, 10, 10);
-      try {
-        Thread.sleep(16);
-      } catch (Exception x) {
-
-      }
-    }
-  }
-}
-
-// This needs to be a midlet in order to have access to the J2ME socket API.
-
-public class SocketBench extends MIDlet {
-
   void runBenchmark() {
-    Display d = Display.getDisplay(this);
-    SocketGame g = new SocketGame();
-    d.setCurrent(g);
-
-    for (int i = 0; i < 16; i++) {
-      Thread thread = new Thread(new DoStuff(), "T" + i);
-      thread.start();
+    try {
+      benchmarkLargeRead();
+    } catch (IOException e) {
+      System.out.println("Exception unexpected: " + e);
+      System.out.println("Make sure the test HTTP server is running: python tests/httpServer.py");
     }
   }
 
